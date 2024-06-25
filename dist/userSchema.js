7db898d87e221a584c5ff9c76a85a500e4300840
wadjoh <wadej93@gmail.com>
2024-06-25 12:49:44 -0700

    again

--- dist/userSchema.js
+++
@@ -1,9 +0,0 @@
-import * as v from 'valibot';
-import { UserToAuthRelationSchema } from './userToAuthSchema.js';
-export const UserSchema = v.lazy(() => v.object({
-    firstName: v.string(),
-    $relations: v.object({
-        UserToCredentials: v.lazy(() => UserToAuthRelationSchema),
-    }),
-}));
-//# sourceMappingURL=userSchema.js.map
